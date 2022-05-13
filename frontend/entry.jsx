926f8c81abb743baba9b6106e6403a11b2abf408
chuck <you@example.com>
2022-05-13 16:25:12 -0700

    React/Rails/Redux setup

---
+++ frontend/entry.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  let root = document.getElementById("root")
+  console.log(root)
+  ReactDOM.render(<h1> Under Construction - Come Back Soon!</h1>,root)
+
+})
