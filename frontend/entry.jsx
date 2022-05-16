a8560dee4e8d7c128b14d967ae1058a94410286a
chuck <you@example.com>
2022-05-16 15:44:17 -0700

    Shaping state

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import {signup} from './actions/session'
 
 document.addEventListener("DOMContentLoaded", () => {
   let root = document.getElementById("root")
+  let store = configureStore()
+   window.store = store
+  window.signup = signup
   console.log(root)
   ReactDOM.render(<h1> Under Construction - Come Back Soon!</h1>,root)
 
