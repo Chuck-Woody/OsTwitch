bcbb4df3611bf66a4b44c6725922fa1d2f4011c6
chuck <you@example.com>
2022-05-20 09:11:57 -0700

    Add login persistence

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,7 +5,7 @@ class Api::UsersController < ApplicationController
 
     if @user.save
       login!(@user)
-      render :show # name of the user show page
+      render :user # name of the user show page
     else
       render json: @user.errors.full_messages, status: 422 # the errors come from the errors generated when the model is validated on a create or update action.
     end
--- frontend/components/nav_bar/nav_bar_container.js
+++ frontend/components/nav_bar/nav_bar_container.js
@@ -4,10 +4,12 @@ import {logout} from '../../actions/session_actions';
 import {openModal} from '../../actions/modal_actions'
 
 const mSTP = (state,ownProps) => {
-  console.log("CurrentUser" , state.session.currentUser)
-  console.log('OwnProps',ownProps)
+  if (state.currentUser) {console.log("User State Slice" , state.entities.users[state.session.id].username)}
+  console.log("User State Slice" , state.session.id)
+  // console.log('OwnProps',ownProps)
 return ({
-  currentUser: state.session.currentUser
+  currentUser: state.session.currentUserId,
+  userInfo: state.entities.users[state.session.currentUserId]
 })}
 
 
