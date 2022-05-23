3d7fafe9c10141b0b9a206f5b12fd51219ec4079
chuck <you@example.com>
2022-05-22 19:45:25 -0700

    add dummy sidebar component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -10,12 +10,13 @@ import {
 import HomePage from './homepage';
 import Modal from './modal/modal'
 import NavBar from "./nav_bar/nav_bar_container";
+import SideBar from './side_bar/side_bar';
 
 const App = () => (
   <div>
     <Modal /> 
     <NavBar />
-    
+    <SideBar />
     <Switch>
       <Route to="/" component={HomePage} />
       <Redirect to="/" />
--- frontend/components/side_bar/side_bar.jsx
+++ frontend/components/side_bar/side_bar.jsx
@@ -3,3 +3,46 @@ import React from "react";
 //convert to a class component once the container has been completed.
 
 
+class SideBar extends React.Component {
+  constructor(props){
+    super(props)
+
+  }
+  render() {
+
+    return (
+      <div className='channel-container' >
+        <div className='channel-header-container'>
+          <h2 className='channel-header-text'>Followed Channels</h2>
+        </div>
+        <div className="channel-item-container"> {/* These will eventually be replaced with a React Component called SideBarItem*/}
+          <div className="avatar-container">
+            <img className="channel-logo" src="https://robohash.org/placeholder1.png"></img>
+          </div>
+          <div className="channel-info-container">
+            <div className='channel-name'>Channel-Name</div>
+            <div className="channel-category">Category</div>
+          </div>
+        </div>
+
+        <div className='channel-header-container'>
+          <h2 className='channel-header-text'>Recommended Channels</h2>
+        </div>
+        <div className="channel-item-container"> {/* These will eventually be replaced with a React Component called SideBarItem*/}
+          <div className="avatar-container">
+            <img className="channel-logo" src="https://robohash.org/placeholder2.png"></img>
+          </div>
+          <div className="channel-info-container">
+            <div className='channel-name'>Channel-Name</div>
+            <div className="channel-category">Category</div>
+          </div>
+        </div>
+        
+      </div>
+      
+
+    )
+  }
+}
+
+export default SideBar
