6406a900e01221bc3812b47e44cfbcc4d6e1d6e2
chuck <you@example.com>
2022-08-29 18:05:57 -0700

    add preview toggle

--- frontend/components/channel/channel_edit.jsx
+++ frontend/components/channel/channel_edit.jsx
@@ -4,6 +4,13 @@ import UnderStreamComponent from './under_stream'
 class ChannelEdit extends React.Component {
   constructor(props){
     super(props)
+    this.state = {
+      displayPreview: true
+    }
+  }
+
+  displayPreview() {
+    this.setState({displayPreview: !this.state.displayPreview},() => (this.state.displayPreview))
   }
 
   componentDidMount(){
@@ -12,15 +19,11 @@ class ChannelEdit extends React.Component {
   render() {
     console.log(this.props)
     let {darkTheme} = this.props;
-    console.log(Object.values(this.props.allChannels))
-    return (
-      <div className={ `${darkTheme ? 'dark-theme': 'light-theme'} channel-edit-background`}>
-        <div className='preview-toggle-button-container'>
-          <button className={`${darkTheme ? 'dark-theme': 'light-theme'} preview-toggle`} >
-            Show Preview
-          </button>
-        </div>
-        <div className="understream-about-preview">
+    let preview
+    
+    if (this.state.displayPreview) {
+      console.log(`If this value is true then the preview should show: ${this.state.displayPreview}`);
+      preview = (<div className="understream-about-preview">
           <div className={` ${darkTheme ? 'dark-theme' : 'light-theme'} channel-about-background`}>
             <div className="about-streamer">
               { `About ${this.props.allChannels[this.props.currentChannel].channel_name}` }
@@ -32,6 +35,20 @@ class ChannelEdit extends React.Component {
             { `${this.props.allChannels[this.props.currentChannel].channel_description}` }
             </div>
           </div>
+        </div>)
+    } 
+    return (
+      <div className={ `${darkTheme ? 'dark-theme': 'light-theme'} channel-edit-background`}>
+        <div className='preview-toggle-button-container'>
+          <button onClick={()=>this.displayPreview()} className={`${darkTheme ? 'dark-theme': 'light-theme'} preview-toggle`} >
+            Show Preview
+          </button>
+        </div>
+        {preview}
+
+        <div className="profile-settings-header-container">
+          <h3 className="profile-settings-header">Profile Settings</h3>
+          <div className="profile-settings-underheader">Change identifying details for your account</div>
         </div>
       </div>
     )
